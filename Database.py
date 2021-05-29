749f4de529bfbd85354d2757c7fc0936ba0bfce8
Eren Gul <erengul321@gmail.com>
2021-05-29 12:58:22 +0200

    base project

---
+++ Database.py
@@ -0,0 +1,18 @@
+import sqlite3
+
+# Step 0 Data Preparation
+# Create a database
+connection = sqlite3.connect('Lesson2.db')
+cur = connection.cursor()
+# Create table
+cur.execute('''CREATE TABLE users ( username varchar(30), admin boolean);''')
+
+# Insert a row of data
+cur.execute("INSERT INTO users (username, admin) VALUES ('ran', true),('haki', false);")
+
+# Save (commit) the changes
+connection.commit()
+
+# We can also close the connection if we are done with it.
+# Just be sure any changes have been committed or they will be lost.
+connection.close()
