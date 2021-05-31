3adc95ff4f3fb048b413d7f477a5d68e2dc01d1c
Tarik <thh.ualit@gmail.com>
2021-05-31 15:34:14 +0200

    added options

--- CreateUserAccount.py
+++ CreateUserAccount.py
@@ -10,11 +10,22 @@ class CreateUserAccount:
 
 
     def tableEntries(self):
-        question1 = raw_input('Hello \nWould you like to login or register? (plz answer with login or register )')
-        if(question1.lower().strip() == 'login'):
-            LoginUser().login()
+        question1 = raw_input('Hello \nWould you like to login or register? (plz answer with login or register) ')
+        while(question1.lower().strip() != 'exit'):
 
-        elif(question1.lower().strip() == 'register'):
-            RegisterNewUser().registerUser()
+            if(question1.lower().strip() == 'login'):
+                LoginUser().login()
+                question1 = raw_input('Would you like to login or register or exit? ')
+
+            elif(question1.lower().strip() == 'register'):
+                RegisterNewUser().registerUser()
+                question1 = raw_input('Would you like to login or exit? ')
+
+            else:
+                print('That is not a valid input.')
+                question1 = raw_input(
+                    'Would you like to login or register? (plz answer with login or register ) enter exit if oyu want to exit ')
+
+
 
 
